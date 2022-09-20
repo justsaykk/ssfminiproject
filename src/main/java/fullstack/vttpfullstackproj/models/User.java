4683acd79f4306bdc51a260de62af55a21c975d3
justsaykk <kaikein.woo@gmail.com>
2022-09-20 16:45:15 +0800

    Second firing was due to a blank image initiated by a image tag src

--- src/main/java/fullstack/vttpfullstackproj/models/User.java
+++ src/main/java/fullstack/vttpfullstackproj/models/User.java
@@ -1,5 +1,6 @@
 package fullstack.vttpfullstackproj.models;
 
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -46,6 +47,15 @@ public class User {
         this.country = country;
     }
 
+    public Boolean urlValidator(String url) {
+        try {
+            new URL(url).toURI();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
     public JsonObject toJsonObject() {
         return Json.createObjectBuilder()
                 .add(this.email, Json.createObjectBuilder()
@@ -65,10 +75,13 @@ public class User {
     }
 
     public void setUser(MultiValueMap<String, String> form) {
+        String profilePicUrl = form.getFirst("profilePicUrl").toLowerCase();
         this.name = form.getFirst("name").toLowerCase();
         this.email = form.getFirst("email").toLowerCase();
         this.country = form.containsKey("country") ? form.getFirst("country").toLowerCase() : "unknown";
-        this.profilePic = form.getFirst("profilePicUrl").toLowerCase();
+        this.profilePic = (urlValidator(profilePicUrl))
+                ? profilePicUrl
+                : "https://media.istockphoto.com/vectors/thumbnail-image-vector-graphic-vector-id1147544807?k=20&m=1147544807&s=612x612&w=0&h=pBhz1dkwsCMq37Udtp9sfxbjaMl27JUapoyYpQm0anc=";
     }
 
     public void setOldUser(MultiValueMap<String, String> form) {
